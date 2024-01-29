8cb3a2c683dcfeebff2b7374338ec1e04d42d99c
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-01-29 22:27:18 +0530

    fixed basics code

--- src/practiceQuestions/linkedList/basics.java
+++ src/practiceQuestions/linkedList/basics.java
@@ -10,7 +10,10 @@ class LinkedList{
         private Node prev;
         private int data;
         Node(int value){
-            this.next = null;
+            this.data = value;
+        }
+        Node(int value, Node next){
+            this.next = next;
             this.data = value;
         }
     }
@@ -21,38 +24,30 @@ class LinkedList{
         if (tail == null){
             tail = head;
         }
-        size += 1;
+        size++;
     }
     void insertLast(int data){
         Node node = new Node(data);
         node.prev = tail;
         tail.next = node;
         tail = node;
-        size += 1;
+        size++;
     }
-    void add(int data, int index){
-        if (index >= size || index < 0){
-            return;
-        }
+    void insert(int data, int index){
         if (index == 0){
             insertFirst(data);
             return;
         }
-        if (index == size - 1){
+        else if (index == size){
             insertLast(data);
             return;
         }
-        int i = 0;
         Node traversalNode = head;
-        while(i < index - 1){
+        for (int i = 1;i < index;i++){
             traversalNode = traversalNode.next;
-            i++;
         }
-        Node newNode = new Node(data);
-        traversalNode.next = newNode;
-        traversalNode.next.next =newNode;
-        tail = traversalNode.next;
-        size += 1;
+        traversalNode.next = new Node(data, traversalNode.next);
+        size++;
     }
     void removeFirst(){
         if (head == null) return;
@@ -61,29 +56,32 @@ class LinkedList{
             return;
         }
         head = head.next;
+        size--;
     }
     void removeLast(){
         if (tail == null) return;
-        head = head.next;
+        if (tail.prev == null){
+            tail = head = null;
+            return;
+        }
+        tail = tail.prev;
+        size--;
     }
-    void delete(int data){
-        if (head == null){
+    void delete(int index){
+        if(index == 0){
+            removeFirst();
+            return;
+        }
+        else if(index == size){
+            removeLast();
             return;
         }
         Node traversalNode = head;
-        if (traversalNode.data == data){
-            head = traversalNode.next;
-            size -= 1;
-            return;
-        }
-        while(traversalNode.next != null){
-            if(traversalNode.next.data == data){
-                traversalNode.next = traversalNode.next.next;
-                size -= 1;
-                break;
-            }
+        for(int i = 1;i<index;i++){
             traversalNode = traversalNode.next;
         }
+        traversalNode.next = traversalNode.next.next;
+        size--;
     }
     void printLL(){
         Node traversalNode = head;
@@ -91,7 +89,6 @@ class LinkedList{
             System.out.print(traversalNode.data + " -> ");
             traversalNode = traversalNode.next;
         }
-
         System.out.print("end");
     }
 }
@@ -100,7 +97,7 @@ class DoublyLinkedList{
 
     private Node head;
     private Node tail;
-    int length = 0;
+    int size = 0;
     private class Node{
         private Node next;
         private Node prev;
@@ -108,41 +105,85 @@ class DoublyLinkedList{
         Node(int value){
             this.data = value;
         }
+        Node(int value, Node next){
+            this.data = value;
+            this.next = next;
+        }
     }
-    void add(int data){
-        if (head == null){
-            head = new Node(data);
-            head.prev = null;
+    void insertFirst(int data){
+        Node node = new Node(data);
+        node.next = head;
+        node.prev = null;
+        if (head != null){
+            head.prev = node;
+        }
+        head = node;
+        if(head.next == null){
+            tail = node;
+        }
+        size++;
+    }
+    void insertLast(int data){
+        Node last = new Node(data);
+        tail.next = last;
+        last.prev = tail;
+        tail = last;
+        size++;
+    }
+    void insert(int data, int index){
+        if (index == 0){
+            insertFirst(data);
+            return;
+        }
+        else if (index == size){
+            insertLast(data);
             return;
         }
         Node traversalNode = head;
-        while(traversalNode.next != null){
+        for (int i = 1;i < index;i++){
             traversalNode = traversalNode.next;
         }
-        traversalNode.next = new Node(data);
+        Node newNode = new Node(data, traversalNode.next);
+        newNode.next.prev = newNode;
+        traversalNode.next = newNode;
+        newNode.prev = traversalNode;
+        size++;
+    }
+    void removeFirst(){
+        if (head == null) return;
+        if (head.next == null){
+            head = tail = null;
+            return;
+        }
+        head = head.next;
+        size--;
+    }
+    void removeLast(){
+        if (tail == null) return;
+        if (tail.prev == null){
+            tail = head = null;
+            return;
+        }
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+    }
+    void delete(int index){
+        if(index == 0){
+            removeFirst();
+            return;
+        }
+        else if(index == size){
+            removeLast();
+            return;
+        }
+        Node traversalNode = head;
+        for(int i = 1;i<index;i++){
+            traversalNode = traversalNode.next;
+        }
+        traversalNode.next = traversalNode.next.next;
         traversalNode.next.prev = traversalNode;
-        tail = traversalNode.next;
-        length += 1;
-    }
-    void delete(int data){
-        if (head == null){
-            return;
-        }
-        Node traversalNode = head;
-        if (traversalNode.data == data){
-            head = traversalNode.next;
-            length -= 1;
-            return;
-        }
-        while(traversalNode.next != null){
-            if(traversalNode.next.data == data){
-                traversalNode.next = traversalNode.next.next;
-                traversalNode.next.prev = traversalNode;
-                length -= 1;
-                break;
-            }
-            traversalNode = traversalNode.next;
-        }
+        size--;
     }
     void printLL(){
         Node traversalNode = head;
@@ -150,12 +191,11 @@ class DoublyLinkedList{
             System.out.print(traversalNode.data + " -> ");
             traversalNode = traversalNode.next;
         }
-
         System.out.print("end");
     }
     void reversePrint(){
         Node traversalNode = tail;
-        while(traversalNode.prev != null){
+        while(traversalNode != null){
             System.out.print(traversalNode.data + " -> ");
             traversalNode = traversalNode.prev;
         }
@@ -166,7 +206,7 @@ class DoublyLinkedList{
 class CircularLinkedList{
     private Node head;
     private Node tail;
-    int length = 0;
+    int size = 0;
     private class Node{
         private Node next;
         private Node prev;
@@ -174,77 +214,105 @@ class CircularLinkedList{
         Node(int value){
             this.data = value;
         }
+        Node(int value, Node next){
+            this.data = value;
+            this.next = next;
+        }
     }
-    void add(int data){
-        if (head == null){
-            head = new Node(data);
-            tail = head;
+    void insertFirst(int data){
+        Node node = new Node(data);
+        node.next = head;
+        node.prev = null;
+        if (head != null){
+            head.prev = node;
+        }
+        head = node;
+        if(head.next == null){
+            tail = node;
+            tail.next = head;
+        }
+        size++;
+    }
+    void insertLast(int data){
+        Node last = new Node(data);
+        last.next = head;
+        tail.next = last;
+        last.prev = tail;
+        tail = last;
+        head.prev = tail;
+        size++;
+    }
+    void insert(int data, int index){
+        if (index == 0){
+            insertFirst(data);
+            return;
+        }
+        else if (index == size){
+            insertLast(data);
             return;
         }
         Node traversalNode = head;
-        while(traversalNode != tail){
+        for (int i = 1;i < index;i++){
             traversalNode = traversalNode.next;
         }
-        traversalNode.next = new Node(data);
-        traversalNode.next.next = head;
+        Node newNode = new Node(data, traversalNode.next);
+        newNode.next.prev = newNode;
+        traversalNode.next = newNode;
+        newNode.prev = traversalNode;
+        size++;
+    }
+    void removeFirst(){
+        if (head == null) return;
+        if (head.next == null){
+            head = tail = null;
+            return;
+        }
+        tail.next = head.next;
+        head = head.next;
+        size--;
+    }
+    void removeLast(){
+        if (tail == null) return;
+        if (tail.prev == null){
+            tail = head = null;
+            return;
+        }
+        tail = tail.prev;
+        head.prev = tail;
+//        tail.next = head.next;
+        size--;
+    }
+    void delete(int index){
+        if(index == 0){
+            removeFirst();
+            return;
+        }
+        else if(index == size){
+            removeLast();
+            return;
+        }
+        Node traversalNode = head;
+        for(int i = 1;i<index;i++){
+            traversalNode = traversalNode.next;
+        }
+        traversalNode.next = traversalNode.next.next;
         traversalNode.next.prev = traversalNode;
-        tail = traversalNode.next;
-        length += 1;
-    }
-    void delete(int data){
-        if (head == null){
-            return;
-        }
-        Node traversalNode = head;
-        if (traversalNode.data == data){
-            tail.next = tail.next.next;
-            head = traversalNode.next;
-            length -= 1;
-            return;
-        }
-        while(traversalNode.next != head && traversalNode.next != null){
-            if(traversalNode.next.data == data){
-                traversalNode.next = traversalNode.next.next;
-                traversalNode.next.prev = traversalNode;
-                length -= 1;
-                break;
-            }
-            traversalNode = traversalNode.next;
-        }
+        size--;
     }
     void printLL(){
-        if (head == null){
-            return;
-        }
-        System.out.print(head.data + " -> ");
         Node traversalNode = head;
-        while(traversalNode != head.next){
+        do{
             System.out.print(traversalNode.data + " -> ");
             traversalNode = traversalNode.next;
-        }
+        }while(traversalNode != head);
         System.out.print("end");
     }
     void reversePrint(){
-        if (tail == null){
-            return;
-        }
-        System.out.print(tail.data + " -> ");
-        Node traversalNode = tail.prev;
-        while(traversalNode.prev != head.next){
-            System.out.print(traversalNode.data + " -> ");
-            traversalNode = traversalNode.prev;
-        }
-        System.out.print("end");
-    }
-    void print(){
-        if (tail == null){
-            return;
-        }
         Node traversalNode = tail;
-        while(traversalNode.next != null){
+        do{
             System.out.print(traversalNode.data + " -> ");
             traversalNode = traversalNode.prev;
-        }
+        }while(traversalNode != tail);
         System.out.print("end");
     }
 }
@@ -252,75 +320,108 @@ class CircularLinkedList{
 public class basics {
     public static void main(String[] args){
 //        LinkedList l = new LinkedList();
-//        System.out.println(l.length);
-//        l.add(5);
-//        l.add(4);
-//        l.add(3);
-//        l.add(2);
-//        l.add(1);
-//        l.add(5);
-//        l.add(6);
-//        l.add(7);
-//        l.add(8);
-//        l.add(9);
-//        l.add(10);
-//        l.add(11);
+//        System.out.println(l.size);
+//        l.insertFirst(15);
+//        l.insertFirst(20);
+//        l.insertLast(15);
+//        l.insertLast(20);
+//        l.insert(5, 0);
+//        l.insert(4, 1);
+//        l.insert(3, 2);
+//        l.insert(2, 3);
+//        l.insert(1, 4);
+//        l.insert(5, 5);
+//        l.insert(6, 6);
+//        l.insert(7, 7);
+//        l.insert(8, 8);
+//        l.insert(9, 9);
+//        l.printLL();
+//        l.removeFirst();
+//        System.out.println();
+//        System.out.println(l.size);
+//        l.delete(5);
+//        System.out.println();
+//        System.out.println(l.size);
+//        l.removeLast();
+//        System.out.println();
+//        System.out.println(l.size);
 //        l.printLL();
 //        l.delete(5);
 //        System.out.println();
-//        System.out.println(l.length);
-//        l.printLL();
-//        l.delete(5);
-//        System.out.println();
-//        System.out.println(l.length);
+//        System.out.println(l.size);
 //        l.printLL();
 //        // Double linked list
 //        DoublyLinkedList dll = new DoublyLinkedList();
-//        System.out.println(l.length);
-//        dll.add(5);
-//        dll.add(4);
-//        dll.add(3);
-//        dll.add(2);
-//        dll.add(1);
-//        dll.add(5);
-//        dll.add(6);
-//        dll.add(7);
-//        dll.add(8);
-//        dll.add(9);
-//        dll.add(10);
-//        dll.add(11);
+//        System.out.println(dll.size);
+//        dll.insertFirst(5);
+//        dll.insertFirst(4);
+//        dll.insert(3, 0);
+//        dll.insert(2, 1);
+//        dll.insert(1, 2);
+//        dll.insert(5, 3);
+//        dll.printLL();
+//        System.out.println();
+//        dll.removeFirst();
+//        dll.printLL();
+//        System.out.println();
+//        dll.removeLast();
+//        dll.delete(2);
+//        dll.printLL();
+//        System.out.println();
+//        dll.reversePrint();
+////        System.out.println();
+////        dll.printLL();
+//        dll.insertLast(5);
+//        dll.insertLast(6);
+//        System.out.println();
+//        dll.printLL();
+//        System.out.println();
+//        dll.reversePrint();
+//        dll.insert(8, 2);
+//        dll.insert(9, 1);
+//        dll.insert(10, 1);
+//        dll.insert(11,2);
 //        dll.printLL();
 //        dll.delete(5);
 //        System.out.println();
-//        System.out.println(l.length);
+//        System.out.println(dll.size);
 //        dll.printLL();
 //        dll.delete(5);
 //        System.out.println();
-//        System.out.println(l.length);
+//        System.out.println(dll.size);
 //        dll.printLL();
 //        System.out.println();
 //        dll.reversePrint();
         // Circular linked list
         CircularLinkedList cll = new CircularLinkedList();
 //        System.out.println(cll.length);
-        cll.add(5);
-        cll.add(4);
-        cll.add(3);
-        cll.add(2);
-        cll.add(1);
+        cll.insertFirst(5);
+        cll.insertFirst(4);
+        cll.insertFirst(3);
+        cll.insertLast(2);
+        cll.insertLast(1);
+        cll.insert(10, 3);
+        cll.insert(11, 3);
+
 //        cll.add(5);
         cll.printLL();
-//        cll.delete(5);
+        cll.delete(5);
+        cll.removeFirst();
+        cll.removeLast();
+        cll.removeFirst();
+        cll.removeLast();
+        cll.removeLast();
         System.out.println();
+        cll.reversePrint();
 //        System.out.println(cll.length);
 //        cll.printLL();
 //        cll.delete(5);
 //        System.out.println();
 //        System.out.println(cll.length);
-        cll.printLL();
-        System.out.println();
-        cll.reversePrint();
-        System.out.println();
-        cll.print();
+//        cll.printLL();
+//        System.out.println();
+//        cll.reversePrint();
+//        System.out.println();
+//        cll.printLL();
     }
 }
