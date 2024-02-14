96cc8e130e473a5f44bcdca582e602df2921355c
AmanPr33tS1ngh <amanpr333tsingh01@gmail.com>
2024-02-14 22:25:36 +0530

    one ques

--- src/practiceQuestions/tree/treeBasics.java
+++ src/practiceQuestions/tree/treeBasics.java
@@ -223,4 +223,13 @@ public class treeBasics {
         }
         return a;
     }
+    static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null)return null;
+        if(root.val == p.val || root.val == q.val)return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null)return right;
+        if(right == null)return left;
+        return root;
+    }
 }
